4645ef19d1054a43367c0df3d47b97315afc5883
Nishanth Ramesh <35870845+NishiCodes@users.noreply.github.com>
2019-05-24 07:13:48 +0530

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -6,6 +6,7 @@ const provider = new HDWalletProvider(
   'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
   'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
